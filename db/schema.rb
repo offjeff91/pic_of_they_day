4c69693acedf4424805123287dcdd452478c5e6f
Jefferson Fernandes <offjeff91@gmail.com>
2023-02-01 22:12:55 -0300

    feat: add picture resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_02_010721) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_011159) do
+  create_table "pictures", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
